d0e9f7d694fd25414774031062ee79869ac2bef0
Scott Addie <scott.addie@microsoft.com>
2019-05-14 14:01:07 -0500

    Initial commit

---
+++ src/ContosoPets.Ui/Areas/Admin/Pages/Index.cshtml.cs
@@ -0,0 +1,38 @@
+﻿using ContosoPets.Ui.Models;
+using ContosoPets.Ui.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace ContosoPets.Ui.Areas.Admin.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ProductService _productService;
+
+        public IEnumerable<Product> Products { get; private set; } = new List<Product>();
+
+        public IndexModel(ProductService productService)
+        {
+            _productService = productService;
+        }
+
+        public async Task OnGet()
+        {
+            Products = await _productService.GetProducts();
+        }
+
+        public IActionResult OnPostEdit(int productId)
+        {
+            return RedirectToPage("Edit", new { id = productId });
+        }
+
+        public async Task<IActionResult> OnPostDelete(int productId)
+        {
+            await _productService.DeleteProduct(productId);
+
+            return RedirectToPage();
+        }
+    }
+}
---
+++ src/ContosoPets.Ui/Pages/Index.cshtml.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ContosoPets.Ui.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
